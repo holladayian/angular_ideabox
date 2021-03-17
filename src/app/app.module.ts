66561c7e17724f26851bc982ce5ddbf8f856631c
Ian Holladay <holladayian@gmail.com>
2021-03-17 15:40:32 -0600

    Import home and brewery components into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BreweryComponent } from './brewery/brewery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BreweryComponent
   ],
   imports: [
     BrowserModule,
